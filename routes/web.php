78c67fc9f0f1d15cbc8a0f7373b21fded8cca153
SijmenL <lokerssijmen@gmail.com>
2023-10-02 13:23:30 +0200

    werkende login pagina

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/', [HomeController::class, 'home']);
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Auth::routes();
 
