5e1c8de7c563f8abcca55fa95b037d00dbea3a9c
SijmenL <lokerssijmen@gmail.com>
2023-10-09 19:51:23 +0200

    footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,16 @@
+@section('footer')
+    <div class="footer bg-white shadow-sm p-4">
+        <div class="flex-wrap justify-content-between d-flex">
+            <div>
+                <h2>List It!</h2>
+            </div>
+            <div>
+                <p>A website made for programmeren 5</p>
+            </div>
+        </div>
+    </div>
+    <div style="background-color: #efefef" class="p-4 shadow-sm d-flex justify-content-center">
+        <p>&copy;Sijmen Lokers 2023</p>
+    </div>
+
+@endsection
